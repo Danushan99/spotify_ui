eec8c51fa836665058106d80c33a59d00414dff3
Dhanushan99 <it20051648@my.sliit.lk>
2021-10-23 17:58:40 +0530

    nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
+// ignore_for_file: import_of_legacy_library_into_null_safe
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'package:google_fonts/google_fonts.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,20 +18,66 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         canvasColor: const Color(0xFF191919),
         visualDensity: VisualDensity.adaptivePlatformDensity,
-        //fontFamily: GoogleFonts.montserrat().fontFamily,
+        fontFamily: GoogleFonts.montserrat().fontFamily,
       ),
-     home: const Homepage(),
+      home: Homepage(),
     );
   }
 }
 
 class Homepage extends StatelessWidget {
-  const Homepage({ Key? key }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Scaffold(
+        backgroundColor: const Color(0xFF191919),
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF191919),
+          elevation: 0,
+          actions: const [
+            Padding(
+              padding: EdgeInsets.only(right: 8.0),
+              child: Icon(Icons.settings_outlined),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.transparent,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.white.withOpacity(0.8),
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_filled),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: "Search",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.library_books_outlined),
+              label: "Library",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shop),
+              label: "Premium",
+            ),
+          ],
+        ),
+        body: SingleChildScrollView(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+              Padding(
+                padding: EdgeInsets.only(left: 16.0),
+                child: Text(
+                  "Good afternoon",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ])));
   }
 }
